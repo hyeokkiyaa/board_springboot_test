75794b750b7ffded6824173fad003be303da2534
hyeokkiyaa <khm38607574@gmail.com>
2025-01-15 15:33:27 +0900

    add S3 class

---
+++ src/main/java/com/example/board_study_with_back_friend/config/S3Config.java
@@ -0,0 +1,4 @@
+package com.example.board_study_with_back_friend.config;
+
+public class S3Config {
+}
---
+++ src/main/java/com/example/board_study_with_back_friend/controller/S3Controller.java
@@ -0,0 +1,4 @@
+package com.example.board_study_with_back_friend.controller;
+
+public class S3Controller {
+}
